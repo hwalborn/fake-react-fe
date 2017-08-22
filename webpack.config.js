0dbbd5e8ee09948d86f18d3e57565bc838980762
hwalborn <hwalborn@gmail.com>
2017-08-22 12:59:51 -0400

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: 'src/index.js',
+  output: {
+    path: './bundle.js',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react', 'es2015']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+}
